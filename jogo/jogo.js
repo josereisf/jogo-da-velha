97e7814bfc5b0daa72d9a0365c2561ca21f2e2ac
José Reis <diosep70@gmail.com>
2026-03-03 18:32:24 -0300

    atualização

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -3,21 +3,25 @@ let ponto;
 let pontuacao = 0;
 
 let jogo = [0, 0, 0,
-            0, 0, 0,
-            0, 0, 0];
+    0, 0, 0,
+    0, 0, 0];
 
 function selecionar(posicao) {
     if (vez == 'x') {
         posicao.classList.add("selecionarX");
         vez = "o";
         ponto = 1;
+        document.getElementById("mostrar").setAttribute("class", "unidade selecionarO")
     }
     else {
-        posicao.classList.add("selecionarO")
+        posicao.classList.add("selecionarO");
         vez = "x";
         ponto = 4;
+        document.getElementById("mostrar").setAttribute("class", "unidade selecionarX")
     }
     posicao.removeAttribute("onclick");
+
+
 
     let id = posicao.getAttribute('id');
     jogo[id] = ponto;
@@ -35,21 +39,37 @@ function selecionar(posicao) {
         jogo[0] + jogo[4] + jogo[8] == 3
     ) {
         alert("X ganhou");
+        for (let i = 0; i < 9; i++) {
+            document.getElementById(i).removeAttribute("onclick");
+        }
     }
     else if (jogo[0] + jogo[1] + jogo[2] == 12 ||
-             jogo[3] + jogo[4] + jogo[5] == 12 ||
-             jogo[6] + jogo[7] + jogo[8] == 12 ||
+        jogo[3] + jogo[4] + jogo[5] == 12 ||
+        jogo[6] + jogo[7] + jogo[8] == 12 ||
 
-             jogo[0] + jogo[3] + jogo[6] == 12 ||
-             jogo[1] + jogo[4] + jogo[7] == 12 ||
-             jogo[2] + jogo[5] + jogo[8] == 12 ||
+        jogo[0] + jogo[3] + jogo[6] == 12 ||
+        jogo[1] + jogo[4] + jogo[7] == 12 ||
+        jogo[2] + jogo[5] + jogo[8] == 12 ||
 
-             jogo[2] + jogo[4] + jogo[6] == 12 ||
-             jogo[0] + jogo[4] + jogo[8] == 12
+        jogo[2] + jogo[4] + jogo[6] == 12 ||
+        jogo[0] + jogo[4] + jogo[8] == 12
     ) {
-        console.log("O ganhou");
+        alert("O ganhou");
+        for (let i = 0; i < 9; i++) {
+            document.getElementById(i).removeAttribute("onclick");
+        }
     }
-    else if (pontuacao == 9){
+    else if (pontuacao == 9) {
         alert("Deu velha");
+    }
+}
+function reiniciar() {
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(i).setAttribute("onclick", "selecionar(this)");
+        document.getElementById(i).setAttribute("class", "unidade");
+        jogo = [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0];
+        pontuacao = 0;
     }
 }
